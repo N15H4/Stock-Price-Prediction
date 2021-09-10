bd03ba78267ac512590cd3b714ba0ec0176eaaba
Nisha Kumari <40362085+N15H4@users.noreply.github.com>
2021-09-11 04:06:12 +0530

    Create Stage3_FeatureExtraction.py

---
+++ src/Stage3_FeatureExtraction.py
@@ -0,0 +1,8 @@
+# Part 2 - Building the RNN
+from keras.models import Sequential
+from keras.models import Dense
+from keras.models import LSTM
+from keras.models import Dropout
+
+# Initialising the RNN
+regressor = Sequential()
