3bf328fba6a0b323f1d128c0db2e841dbaa96409
Nisha Kumari <40362085+N15H4@users.noreply.github.com>
2021-09-11 04:52:19 +0530

    Create Stage5_Visualisation.py

---
+++ src/Stage5_Visualisation.py
@@ -0,0 +1,40 @@
+#Visualising the results
+plt.plot(real_stock_price, color = 'red', label = 'Real Google Stock Price')
+plt.plot(predicted_stock_price, color = 'blue', label = 'Predicted Google Stock Price')
+plt.title('Google Stock Price Prediction')
+plt.xlabel('Time')
+plt.ylabel('Google Stock Price')
+plt.legent()
+plt.show()
+
+real_stock_price = dataset_test.iloc[:, 1:2].values
+dataset_test.head()
+dataset_test.info()
+dataset_test["Volume"] = dataset_test["Volume"].str.replace(','', '').astype(float)
+test_set=dataset_test['Open']
+test_set=pd.DataFrame(test_set)
+test_set.info()
+                                                            
+# Getting the predicted stock price of 2017
+dataset_total = pd.concat((dataset['Open'], dataset_test['Open']), axis = 0)
+inputs = dataset_total[len(dataset_total) - len(dataset_test) - 60:].values
+inputs = inputs.reshape(-1, 1)
+inputs = sc.transform(inputs)
+X_test = []
+for i in range(60, 80):
+  X_test.append(inputs[i-60:i, 0])
+X_test = np.array(X_test)
+X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+predicted_stock_price = regressor.predict(X_test)
+predicted_stock_price = sc.inverse_transform(predicted_stock_price)
+predicted_stock_price = pd.DataFrame(predicted_stock_price)
+predicted_stock_price.info()
+                                                            
+# Visualising the results
+plt.plot(real_stock_price, color = 'red', label = 'Real Google Stock Price')
+plt.plot(predicted_stock_price, color = 'blue', label = 'Predicted Google Stock Price')
+plt.title('Google Stock Price Prediction')
+plt.xlabel('Time')
+plt.ylabel('Google Stock Price')
+plt.legend()
+plt.show()
